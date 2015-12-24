a67ee1cffa9d01134b673690e8c3742c07c03109
Reagan Mbitiru <reaganmbitiru@gmail.com>
2015-12-24 01:19:00 -0500

    removed unnecessary included files

--- src/main/com/outstare/arg/processor/adapter/Adapter.java
+++ src/main/com/outstare/arg/processor/adapter/Adapter.java
@@ -1,9 +1,5 @@
 package com.outstare.arg.processor.adapter;
 
-import com.outstare.arg.processor.flag.Flag;
-
-import java.util.ArrayList;
-
 /**
  * Created by IBM Research Lab, Copyright 2015.
  */
